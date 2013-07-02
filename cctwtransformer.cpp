96f10e00bd36a1b73ff618872a573d4dd7b37008
jennings <jennings@anl.gov>
2013-07-02 14:25:06 -0500

    Interface for transformer class

--- cctwtransformer.cpp
+++ cctwtransformer.cpp
@@ -1,5 +1,14 @@
 #include "cctwtransformer.h"
 
-CctwTransformer::CctwTransformer()
+CctwTransformer::CctwTransformer(CctwInputDataInterface *input,
+                                 CctwOutputDataInterface *output,
+                                 CctwTransformInterface *xform,
+                                 int osx, int osy, int osz)
+  : m_InputData(input),
+    m_OutputData(output),
+    m_Transform(xform),
+    m_OversampleX(osx),
+    m_OversampleY(osy),
+    m_OversampleZ(osz)
 {
 }
--- cctwtransformer.h
+++ cctwtransformer.h
@@ -1,10 +1,28 @@
 #ifndef CCTWTRANSFORMER_H
 #define CCTWTRANSFORMER_H
 
+#include "cctwinputdatainterface.h"
+#include "cctwoutputdatainterface.h"
+#include "cctwtransforminterface.h"
+
 class CctwTransformer
 {
 public:
-  CctwTransformer();
+  CctwTransformer(CctwInputDataInterface *input,
+                  CctwOutputDataInterface *output,
+                  CctwTransformInterface *xform,
+                  int osx, int osy, int osz);
+
+public:
+  void transformChunk(int nx, int ny, int nz);
+
+private:
+  CctwInputDataInterface  *m_InputData;
+  CctwOutputDataInterface *m_OutputData;
+  CctwTransformInterface  *m_Transform;
+  int                      m_OversampleX;
+  int                      m_OversampleY;
+  int                      m_OversampleZ;
 };
 
 #endif // CCTWTRANSFORMER_H
