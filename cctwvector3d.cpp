f8eafaa842e788a62682be46ce6c64052c605961
jennings <jennings@anl.gov>
2013-07-03 16:14:54 -0500

    Vector operations defined

--- cctwvector3d.cpp
+++ cctwvector3d.cpp
@@ -8,5 +8,75 @@ CctwVector3D<T>::CctwVector3D(T x, T y, T z)
 {
 }
 
+template <typename T>
+CctwVector3D<T>::CctwVector3D()
+  : m_X(0), m_Y(0), m_Z(0)
+{
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::operator+(const CctwVector3D<T> &vec) const
+{
+  return CctwVector3D<T>(x()+vec.x(), y()+vec.y(), z()+vec.z());
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::operator-(const CctwVector3D<T> &vec) const
+{
+  return CctwVector3D<T>(x()-vec.x(), y()-vec.y(), z()-vec.z());
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::operator*(const CctwVector3D<T> &vec) const
+{
+  return CctwVector3D<T>(x()*vec.x(), y()*vec.y(), z()*vec.z());
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::operator/(const CctwVector3D<T> &vec) const
+{
+  return CctwVector3D<T>(x()/vec.x(), y()/vec.y(), z()/vec.z());
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::operator+(const T val) const
+{
+  return CctwVector3D<T>(x()+val, y()+val, z()+val);
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::operator-(const T val) const
+{
+  return CctwVector3D<T>(x()-val, y()-val, z()-val);
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::operator*(const T val) const
+{
+  return CctwVector3D<T>(x()*val, y()*val, z()*val);
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::operator/(const T val) const
+{
+  return CctwVector3D<T>(x()/val, y()/val, z()/val);
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::min(const CctwVector3D<T> &vec) const
+{
+  return CctwVector3D<T>((x()<vec.x()?x():vec.x()),
+                         (y()<vec.y()?y():vec.y()),
+                         (z()<vec.z()?z():vec.z()));
+}
+
+template <typename T>
+CctwVector3D<T> CctwVector3D<T>::max(const CctwVector3D<T> &vec) const
+{
+  return CctwVector3D<T>((x()>vec.x()?x():vec.x()),
+                         (y()>vec.y()?y():vec.y()),
+                         (z()>vec.z()?z():vec.z()));
+}
+
 template class CctwVector3D<int>;
 template class CctwVector3D<double>;
--- cctwvector3d.h
+++ cctwvector3d.h
@@ -6,11 +6,45 @@ class CctwVector3D
 {
 public:
   CctwVector3D(T x, T y, T z);
+  CctwVector3D();
+
+public:
+  T x() const { return m_X; }
+  T y() const { return m_Y; }
+  T z() const { return m_Z; }
+
+  CctwVector3D<T> operator+(const CctwVector3D<T> &vec) const;
+  CctwVector3D<T> operator-(const CctwVector3D<T> &vec) const;
+  CctwVector3D<T> operator*(const CctwVector3D<T> &vec) const;
+  CctwVector3D<T> operator/(const CctwVector3D<T> &vec) const;
+
+  CctwVector3D<T> operator+(const T val) const;
+  CctwVector3D<T> operator-(const T val) const;
+  CctwVector3D<T> operator*(const T val) const;
+  CctwVector3D<T> operator/(const T val) const;
+
+//  template <typename T2>
+//  CctwVector3D<T> operator+(const CctwVector3D<T2> &vec) const;
+
+//  template <typename T2>
+//  CctwVector3D<T> operator-(const CctwVector3D<T2> &vec) const;
+
+//  template <typename T2>
+//  CctwVector3D<T> operator*(const CctwVector3D<T2> &vec) const;
+
+//  template <typename T2>
+//  CctwVector3D<T> operator/(const CctwVector3D<T2> &vec) const;
+
+  CctwVector3D<T> min(const CctwVector3D<T> &vec) const;
+  CctwVector3D<T> max(const CctwVector3D<T> &vec) const;
 
 private:
   T m_X;
   T m_Y;
   T m_Z;
 };
+
+typedef CctwVector3D<int>    CctwIntVector3D;
+typedef CctwVector3D<double> CctwDoubleVector3D;
 
 #endif // CCTWVECTOR3D_H
