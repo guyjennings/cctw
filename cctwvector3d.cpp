40847459c5cd1376fd450d7c7167a9f59a45a79d
jennings <jennings@anl.gov>
2013-07-03 14:19:20 -0500

    Convert CctwVector3D to a template class

--- cctwvector3d.cpp
+++ cctwvector3d.cpp
@@ -1,5 +1,12 @@
 #include "cctwvector3d.h"
 
-CctwVector3D::CctwVector3D(double x, double y, double z)
+template <typename T>
+CctwVector3D<T>::CctwVector3D(T x, T y, T z)
+  : m_X(x),
+    m_Y(y),
+    m_Z(z)
 {
 }
+
+template class CctwVector3D<int>;
+template class CctwVector3D<double>;
--- cctwvector3d.h
+++ cctwvector3d.h
@@ -1,10 +1,16 @@
 #ifndef CCTWVECTOR3D_H
 #define CCTWVECTOR3D_H
 
+template <typename T>
 class CctwVector3D
 {
 public:
-  CctwVector3D(double x, double y, double z);
+  CctwVector3D(T x, T y, T z);
+
+private:
+  T m_X;
+  T m_Y;
+  T m_Z;
 };
 
 #endif // CCTWVECTOR3D_H
