692fb3ec4752d0b4cb06a2e62c3c7eeb238294ab
Justin M Wozniak <wozniak@mcs.anl.gov>
2013-07-10 14:33:03 -0500

    Sketch additional interface to C++

--- swift/cctw.swift
+++ swift/cctw.swift
@@ -1,4 +1,9 @@
 
+(int r[]) CctwRequisites(int osx, int osy, int osz)
+"cctw" "0.0"
+[ "set <<r>> [ cctw::requisites <<osx>> <<osy>> <<osz>> ]" ];
+
+
 (blob o) CctwTransform(int osx, int osy, int osz, blob i, int count)
 "cctw" "0.0"
 [ "set <<o>> [ cctw::transform <<osx>> <<osy>> <<osz>> <<i>> <<count>> ]" ];
