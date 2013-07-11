3f8892151d2c9cc45ad493a6c6c0ab3bbd8e4022
Justin Wozniak <wozniak@login4.beagle.ci.uchicago.edu>
2013-07-11 16:30:29 -0500

    Support variable number of processes

--- swift/tests/cctw-2.sh
+++ swift/tests/cctw-2.sh
@@ -8,5 +8,7 @@ CCTW_SWIFT_PKG=$(/bin/pwd)
 
 cd tests
 
+N=${N:-3}
+
 stc -O0 -I $CCTW_SWIFT_PKG -r $CCTW_SWIFT_PKG cctw-2.swift cctw-2.tcl
-turbine -l cctw-2.tcl --data=$CCTW_SWIFT_PKG/data
+time turbine -l -n $N cctw-2.tcl --data=$CCTW_SWIFT_PKG/data
