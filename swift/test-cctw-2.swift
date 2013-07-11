abe68f8f418e7842cb338e78bc363fd85ea4268d
Justin M Wozniak <wozniak@mcs.anl.gov>
2013-07-11 14:02:15 -0500

    Just do input here

--- swift/test-cctw-2.swift
+++ swift/test-cctw-2.swift
@@ -33,23 +33,4 @@ main
       }
     }
   }
-
-  foreach i in [0:max_i-1]
-  {
-    foreach j in [0:max_j-1]
-    {
-      foreach k in [0:max_k-1]
-      {
-        int requisiteIDs[] = CctwRequisites(i, j, k);
-        int count = size(requisites);
-        blob requisites[];
-        foreach r, m in requisiteIDs
-        {
-          requisites[m] = r;
-        }
-        outputs[i][j][k] = CctwTransform(i, j, k, requisites, count);
-      }
-    }
-  }
-  CctwStoreOutputs(outputs);
 }
