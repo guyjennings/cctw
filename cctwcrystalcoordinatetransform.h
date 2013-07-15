0098fb143dad25bfd7c661f6a6df166d8ae67b23
jennings <jennings@anl.gov>
2013-07-15 14:52:46 -0500

    Added data members for coordinate transformation

--- cctwcrystalcoordinatetransform.h
+++ cctwcrystalcoordinatetransform.h
@@ -2,6 +2,7 @@
 #define CCTWCRYSTALCOORDINATETRANSFORM_H
 
 #include "cctwtransforminterface.h"
+#include "cctwmatrix3x3.h"
 
 class CctwCrystalCoordinateTransform : public CctwTransformInterface
 {
@@ -17,6 +18,29 @@ public:
                                           // Transform from input to output space coordinates
   virtual CctwDoubleVector3D inverse(CctwDoubleVector3D q);
                                           // Back transform from output to input space coordinates
+
+public:
+  double m_Lambda;
+
+  CctwDoubleMatrix3x3 m_B;
+  CctwDoubleMatrix3x3 m_U;
+  CctwDoubleMatrix3x3 m_G;
+  CctwDoubleMatrix3x3 m_D;
+  CctwDoubleMatrix3x3 m_O;
+
+  CctwDoubleVector3D m_Tds;
+  CctwDoubleVector3D m_Te;
+  CctwDoubleVector3D m_Tpd;
+  CctwDoubleVector3D m_Tgs0;
+
+private:
+  CctwDoubleMatrix3x3 m_Binv;
+  CctwDoubleMatrix3x3 m_Uinv;
+  CctwDoubleMatrix3x3 m_Ginv;
+  CctwDoubleMatrix3x3 m_Dinv;
+  CctwDoubleMatrix3x3 m_Oinv;
+
+  CctwDoubleVector3D m_Tgs;
 };
 
 #endif // CCTWCRYSTALCOORDINATETRANSFORM_H
