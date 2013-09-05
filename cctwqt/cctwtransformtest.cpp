cf1b18e56a455757b26562dd93b231f211bc333c
jennings <jennings@anl.gov>
2013-09-05 15:59:41 -0500

    Added all of Branton's parameters to CctwTransformTest

--- cctwqt/cctwtransformtest.cpp
+++ cctwqt/cctwtransformtest.cpp
@@ -1,21 +1,147 @@
 #include "cctwtransformtest.h"
 #include "cctwqtapplication.h"
+#include <math.h>
 
 CctwTransformTest::CctwTransformTest(CctwqtApplication *app, QObject *parent) :
   CctwqtObject(parent),
   m_Application(app),
+  m_Frame(m_Application->saver(), this, "frame", 5, "Frame Number"),
   m_PixelSize(m_Application->saver(), this, "pixelSize", 0.080567, "Detector Pixel Size (mm)"),
   m_Wavelength(m_Application->saver(), this, "wavelength", 0.153870, "Wavelength (Angstrom)"),
   m_Distance(m_Application->saver(), this, "distance", 365.97, "Sample->Detector Distance (mm)"),
+
   m_UnitCell(m_Application->saver(), this, "unitCell",
              4.04, 4.04, 4.04, 1.570796, 1.570796, 1.570796,
              "Sample Unit Cell (Angstrom,Rad)"),
+
+  m_UBMat(m_Application->saver(), this, "ubMat",
+          -0.001955, 0.177126, -0.172890,
+          -0.247516, -0.000885, 0.001892,
+          0.000736, 0.172899, 0.177127,
+          "UB Matrix"),
+  m_UBMatInv(m_Application->saver(), this, "ubMat", get_UBMat().inverted(), "UB Matrix Inverse"),
+
+  m_Det0x(m_Application->saver(), this, "det0x", 1041.266200, "X beam center (mm)"),
+  m_Det0y(m_Application->saver(), this, "det0y", 989.624800, "Y beam center (mm)"),
+
+  m_Xtrans(m_Application->saver(), this, "xtrans",
+           0.005277, -0.006373, 0.034958, "XTrans (mm) goniometer head frame"),
+
+  m_OrientErrorDetPitch(m_Application->saver(), this, "orientErrorDetPitch",
+                        -0.014305, "Det Pitch Orientation Error (rad)"),
+  m_OrientErrorDetRoll(m_Application->saver(), this, "orientErrorDetRoll",
+                        -0.008063, "Det Roll Orientation Error (rad)"),
+  m_OrientErrorDetYaw(m_Application->saver(), this, "orientErrorDetYaw",
+                        -0.000361, "Det Yaw Orientation Error (rad)"),
+  m_OrientErrorGonPitch(m_Application->saver(), this, "orientErrorGonPitch",
+                        -0.000000, "Gon Pitch Orientation Error (rad)"),
+
+  m_TwoThetaCorrection(m_Application->saver(), this, "twoThetaCorrection", 0.0, "Two Theta Correction (rad)"),
+  m_TwoThetaNom(m_Application->saver(), this, "twoThetaNom", 0.0, "Two Theta Nominal (rad)"),
+  m_TwoThetaAngle(m_Application->saver(), this, "twoThetaAngle", get_TwoThetaNom()+get_TwoThetaCorrection(), "Two Theta Angle (rad)"),
+
+  m_OmegaCorrection(m_Application->saver(), this, "omegaCorrection", 0.003777, "Omega Correction (rad)"),
+  m_OmegaStep(m_Application->saver(), this, "omegaStep", 0.008727, "Omega Step (rad)"),
+  m_OmegaNom(m_Application->saver(), this, "omegaNom", (get_Frame()-0.5)*get_OmegaStep(), "Omega Nominal (rad)"),
+  m_OmegaAngle(m_Application->saver(), this, "omegaAngle", get_OmegaNom()+get_OmegaCorrection(), "Omega Angle (rad)"),
+
+  m_ChiCorrection(m_Application->saver(), this, "chiCorrection", -0.005531, "Chi Correction (rad)"),
+  m_ChiNom(m_Application->saver(), this, "chiNom", 0.0, "Chi Nominal (rad)"),
+  m_ChiAngle(m_Application->saver(), this, "chiAngle", get_ChiNom()+get_ChiCorrection(), "Chi Angle (rad)"),
+
+  m_PhiCorrection(m_Application->saver(), this, "phiCorrection", 0.0, "Phi Correction (rad)"),
+  m_PhiNom(m_Application->saver(), this, "phiNom", 0.0, "Phi Nominal (rad)"),
+  m_PhiAngle(m_Application->saver(), this, "phiAngle", get_PhiNom()+get_PhiCorrection(), "Phi Angle (rad)"),
+
   m_GridOrigin(m_Application->saver(), this, "gridOrigin",
                1.5, -0.5, 0.0, "Grid Origin (recip lattice units)"),
   m_GridBasis(m_Application->saver(), this, "gridBasis",
               1.0, 0.0, 0.0,
               0.0, 0.5, 0.5,
               0.0, 0.5, -0.5, "Grid Basis (recip latt units)"),
-  m_GridBasisInv(m_Application->saver(), this, "gridBasisInv", get_GridBasis().inverted(), "Grid Basis Inverse")
+  m_GridBasisInv(m_Application->saver(), this, "gridBasisInv", get_GridBasis().inverted(), "Grid Basis Inverse"),
+  m_OMat(m_Application->saver(), this, "oMat",
+         0.0, 0.0, -1.0,
+         -1.0, 0.0, 0.0,
+         0.0, 1.0, 0.0, "o Matrix - rotate detector axes into lab"),
+  m_OMatInv(m_Application->saver(), this, "oMatInv", get_OMat().inverted(), "o Matrix Inverse"),
+  m_DMat(m_Application->saver(), this, "dMat", CctwDoubleMatrix3x3(), "d Matrix - lab -> detector coords"),
+  m_DMatInv(m_Application->saver(), this, "dMatInv", CctwDoubleMatrix3x3(), "d Matrix Inverse"),
+  m_UMat(m_Application->saver(), this, "uMat", CctwDoubleMatrix3x3(), "u Matrix"),
+  m_UMatInv(m_Application->saver(), this, "uMatInv", CctwDoubleMatrix3x3(), "u Matrix Inverse"),
+  m_BMat(m_Application->saver(), this, "bMat", CctwDoubleMatrix3x3(), "b Matrix"),
+  m_BMatInv(m_Application->saver(), this, "bMatInv", CctwDoubleMatrix3x3(), "b Matrix Inverse"),
+  m_GMat(m_Application->saver(), this, "gMat", CctwDoubleMatrix3x3(), "g Matrix"),
+  m_GMatInv(m_Application->saver(), this, "gMatInv", CctwDoubleMatrix3x3(), "g Matrix Inverse"),
+  m_CD(m_Application->saver(), this, "cD", CctwDoubleVector3D(), "cD - crystal position from detector in lab frame"),
+  m_ND(m_Application->saver(), this, "nD", CctwDoubleVector3D(), "nD - detector plane normal direction")
 {
+  CctwDoubleMatrix3x3 dimat = CctwDoubleMatrix3x3::identity();
+
+  dimat = CctwDoubleMatrix3x3::rotZ(get_OrientErrorDetYaw())*dimat;
+  dimat = CctwDoubleMatrix3x3::rotY(get_OrientErrorDetPitch())*dimat;
+  dimat = CctwDoubleMatrix3x3::rotX(get_OrientErrorDetRoll())*dimat;
+  dimat = CctwDoubleMatrix3x3::rotZ(get_TwoThetaAngle())*dimat;
+  dimat = CctwDoubleMatrix3x3::rotY(get_OrientErrorGonPitch())*dimat;
+
+  set_DMatInv(dimat);
+  set_DMat(dimat.inverted());
+
+  CctwDoubleMatrix3x3 gmat = CctwDoubleMatrix3x3::identity();
+
+  gmat = CctwDoubleMatrix3x3::rotZ(get_PhiAngle())*gmat;
+  gmat = CctwDoubleMatrix3x3::rotX(get_ChiAngle())*gmat;
+  gmat = CctwDoubleMatrix3x3::rotZ(get_OmegaAngle())*gmat;
+  gmat = CctwDoubleMatrix3x3::rotY(get_OrientErrorGonPitch())*gmat;
+
+  set_GMat(gmat);
+  set_GMatInv(gmat.inverted());
+
+  set_UBMatInv(get_UBMat().inverted());
+
+  set_BMat(createBMatrix(get_UnitCell()));
+  set_BMatInv(get_BMat().inverted());
+
+  set_UMat(get_UBMat()*get_BMatInv());
+  set_UMatInv(get_UMat().inverted());
+
+  CctwDoubleVector3D cD(get_Distance(), 0, 0);
+  cD = CctwDoubleMatrix3x3::rotZ(get_TwoThetaAngle())*cD;
+  cD = CctwDoubleMatrix3x3::rotY(get_OrientErrorGonPitch())*cD;
+  CctwDoubleVector3D tVec = get_GMat()*get_Xtrans();
+
+  cD = tVec - cD;
+
+  CctwDoubleVector3D nD(dimat(0,0), dimat(1,0), dimat(2,0));
+
+  set_CD(cD);
+  set_ND(nD);
+}
+
+CctwDoubleMatrix3x3 CctwTransformTest::createBMatrix(const CctwUnitCell &cell) const
+{
+  double alat = cell.a();
+  double blat = cell.b();
+  double clat = cell.c();
+  double alpha = cell.alpha();
+  double beta = cell.beta();
+  double gamma = cell.gamma();
+
+  CctwDoubleMatrix3x3 zzz;
+
+  zzz(0,0) = alat;
+  zzz(1,0) = 0;
+  zzz(2,0) = 0;
+
+  zzz(0,1) = blat*cos(gamma);
+  zzz(1,1) = blat*sin(gamma);
+  zzz(2,1) = 0;
+
+  zzz(0,2) = clat*cos(beta);
+  zzz(1,2) = clat*(cos(alpha) - cos(beta)*cos(gamma))/sin(gamma);
+  zzz(1,3) = sqrt(clat*clat - zzz(0,2)*zzz(0,2) - zzz(1,2)*zzz(1,2));
+
+  CctwDoubleMatrix3x3 tmat = zzz.inverted();
+
+  return tmat.transposed();
 }
--- cctwqt/cctwtransformtest.h
+++ cctwqt/cctwtransformtest.h
@@ -18,8 +18,14 @@ signals:
   
 public slots:
   
+public:
+  CctwDoubleMatrix3x3 createBMatrix(const CctwUnitCell &cell) const;
+
 private:
   CctwqtApplication *m_Application;
+
+  Q_PROPERTY(double frame READ get_Frame WRITE set_Frame)
+  QCEP_DOUBLE_PROPERTY(Frame)
 
   Q_PROPERTY(double pixelSize READ get_PixelSize WRITE set_PixelSize)
   QCEP_DOUBLE_PROPERTY(PixelSize)
@@ -33,6 +39,72 @@ private:
   Q_PROPERTY(CctwqtUnitCell unitCell READ get_UnitCell WRITE set_UnitCell)
   CCTWQT_UNITCELL_PROPERTY(UnitCell)
 
+  Q_PROPERTY(CctwDoubleMatrix3x3 ubMat READ get_UBMat WRITE set_UBMat)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(UBMat)
+
+  Q_PROPERTY(CctwDoubleMatrix3x3 ubMatInv READ get_UBMatInv WRITE set_UBMatInv)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(UBMatInv)
+
+  Q_PROPERTY(double det0x READ get_Det0x WRITE set_Det0x)
+  QCEP_DOUBLE_PROPERTY(Det0x)
+
+  Q_PROPERTY(double det0y READ get_Det0y WRITE set_Det0y)
+  QCEP_DOUBLE_PROPERTY(Det0y)
+
+  Q_PROPERTY(CctwDoubleVector3D xtrans READ get_Xtrans WRITE set_Xtrans)
+  CCTWQT_DOUBLEVECTOR3D_PROPERTY(Xtrans)
+
+  Q_PROPERTY(double orientErrorDetPitch READ get_OrientErrorDetPitch WRITE set_OrientErrorDetPitch)
+  QCEP_DOUBLE_PROPERTY(OrientErrorDetPitch)
+
+  Q_PROPERTY(double orientErrorDetRoll READ get_OrientErrorDetRoll WRITE set_OrientErrorDetRoll)
+  QCEP_DOUBLE_PROPERTY(OrientErrorDetRoll)
+
+  Q_PROPERTY(double orientErrorDetYaw READ get_OrientErrorDetYaw WRITE set_OrientErrorDetYaw)
+  QCEP_DOUBLE_PROPERTY(OrientErrorDetYaw)
+
+  Q_PROPERTY(double orientErrorGonPitch READ get_OrientErrorGonPitch WRITE set_OrientErrorGonPitch)
+  QCEP_DOUBLE_PROPERTY(OrientErrorGonPitch)
+
+  Q_PROPERTY(double twoThetaCorrection READ get_TwoThetaCorrection WRITE set_TwoThetaCorrection)
+  QCEP_DOUBLE_PROPERTY(TwoThetaCorrection)
+
+  Q_PROPERTY(double twoThetaNom READ get_TwoThetaNom WRITE set_TwoThetaNom)
+  QCEP_DOUBLE_PROPERTY(TwoThetaNom)
+
+  Q_PROPERTY(double twoThetaAngle READ get_TwoThetaAngle WRITE set_TwoThetaAngle)
+  QCEP_DOUBLE_PROPERTY(TwoThetaAngle)
+
+  Q_PROPERTY(double omegaCorrection READ get_OmegaCorrection WRITE set_OmegaCorrection)
+  QCEP_DOUBLE_PROPERTY(OmegaCorrection)
+
+  Q_PROPERTY(double omegaStep READ get_OmegaStep WRITE set_OmegaStep)
+  QCEP_DOUBLE_PROPERTY(OmegaStep)
+
+  Q_PROPERTY(double omegaNom READ get_OmegaNom WRITE set_OmegaNom)
+  QCEP_DOUBLE_PROPERTY(OmegaNom)
+
+  Q_PROPERTY(double omegaAngle READ get_OmegaAngle WRITE set_OmegaAngle)
+  QCEP_DOUBLE_PROPERTY(OmegaAngle)
+
+  Q_PROPERTY(double chiCorrection READ get_ChiCorrection WRITE set_ChiCorrection)
+  QCEP_DOUBLE_PROPERTY(ChiCorrection)
+
+  Q_PROPERTY(double chiNom READ get_ChiNom WRITE set_ChiNom)
+  QCEP_DOUBLE_PROPERTY(ChiNom)
+
+  Q_PROPERTY(double chiAngle READ get_ChiAngle WRITE set_ChiAngle)
+  QCEP_DOUBLE_PROPERTY(ChiAngle)
+
+  Q_PROPERTY(double phiCorrection READ get_PhiCorrection WRITE set_PhiCorrection)
+  QCEP_DOUBLE_PROPERTY(PhiCorrection)
+
+  Q_PROPERTY(double phiNom READ get_PhiNom WRITE set_PhiNom)
+  QCEP_DOUBLE_PROPERTY(PhiNom)
+
+  Q_PROPERTY(double phiAngle READ get_PhiAngle WRITE set_PhiAngle)
+  QCEP_DOUBLE_PROPERTY(PhiAngle)
+
   Q_PROPERTY(CctwDoubleVector3D  gridOrigin READ get_GridOrigin WRITE set_GridOrigin)
   CCTWQT_DOUBLEVECTOR3D_PROPERTY(GridOrigin)
 
@@ -42,42 +114,41 @@ private:
   Q_PROPERTY(CctwDoubleMatrix3x3 gridBasisInv READ get_GridBasisInv WRITE set_GridBasisInv)
   CCTWQT_DOUBLEMATRIX3X3_PROPERTY(GridBasisInv)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 umat READ get_Umat WRITE set_Umat)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(Umat)
+  Q_PROPERTY(CctwDoubleMatrix3x3 oMat READ get_OMat WRITE set_OMat)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(OMat)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 umatInv READ get_UmatInv WRITE set_UmatInv)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(UmatInv)
+  Q_PROPERTY(CctwDoubleMatrix3x3 oMatInv READ get_OMatInv WRITE set_OMatInv)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(OMatInv)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 bmat READ get_Bmat WRITE set_Bmat)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(Bmat)
+  Q_PROPERTY(CctwDoubleMatrix3x3 dMat READ get_DMat WRITE set_DMat)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(DMat)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 bmatInv READ get_BmatInv WRITE set_BmatInv)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(BmatInv)
+  Q_PROPERTY(CctwDoubleMatrix3x3 dMatInv READ get_DMatInv WRITE set_DMatInv)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(DMatInv)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 ubmat READ get_UBmat WRITE set_UBmat)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(UBmat)
+  Q_PROPERTY(CctwDoubleMatrix3x3 uMat READ get_UMat WRITE set_UMat)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(UMat)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 ubmatInv READ get_UBmatInv WRITE set_UBmatInv)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(UBmatInv)
+  Q_PROPERTY(CctwDoubleMatrix3x3 uMatInv READ get_UMatInv WRITE set_UMatInv)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(UMatInv)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 gmat READ get_Gmat WRITE set_Gmat)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(Gmat)
+  Q_PROPERTY(CctwDoubleMatrix3x3 bMat READ get_BMat WRITE set_BMat)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(BMat)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 gmatInv READ get_GmatInv WRITE set_GmatInv)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(GmatInv)
+  Q_PROPERTY(CctwDoubleMatrix3x3 bMatInv READ get_BMatInv WRITE set_BMatInv)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(BMatInv)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 dmat READ get_Dmat WRITE set_Dmat)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(Dmat)
+  Q_PROPERTY(CctwDoubleMatrix3x3 gMat READ get_GMat WRITE set_GMat)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(GMat)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 dmatInv READ get_DmatInv WRITE set_DmatInv)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(DmatInv)
+  Q_PROPERTY(CctwDoubleMatrix3x3 gMatInv READ get_GMatInv WRITE set_GMatInv)
+  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(GMatInv)
 
-//  Q_PROPERTY(CctwDoubleMatrix3x3 omat READ get_Omat WRITE set_Omat)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(Omat)
+  Q_PROPERTY(CctwDoubleVector3D cD READ get_CD WRITE set_CD)
+  CCTWQT_DOUBLEVECTOR3D_PROPERTY(CD)
 
-//  Q_PROPERTY(QMatrix3x3 omatInv READ get_OmatInv WRITE set_OmatInv)
-//  CCTWQT_DOUBLEMATRIX3X3_PROPERTY(OmatInv)
-
+  Q_PROPERTY(CctwDoubleVector3D nD READ get_ND WRITE set_ND)
+  CCTWQT_DOUBLEVECTOR3D_PROPERTY(ND)
 };
 
 #endif // CCTWTRANSFORMTEST_H
