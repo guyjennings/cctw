a4eda77252afac9aed9cb8c1930311f138b2177d
jennings <jennings@anl.gov>
2013-09-10 15:42:51 -0500

    Add a CctwqtCrystalCoordinateTransform::report debugging method

--- cctwqt/cctwqtcrystalcoordinatetransform.cpp
+++ cctwqt/cctwqtcrystalcoordinatetransform.cpp
@@ -1,6 +1,16 @@
 #include "cctwqtcrystalcoordinatetransform.h"
 
-CctwqtCrystalCoordinateTransform::CctwqtCrystalCoordinateTransform(CctwCrystalCoordinateParameters *parms, QObject *parent) :
-  CctwCrystalCoordinateTransform(parms)
+CctwqtCrystalCoordinateTransform::CctwqtCrystalCoordinateTransform(CctwqtCrystalCoordinateParameters *parms, QObject *parent) :
+  CctwCrystalCoordinateTransform(parms, parent),
+  m_QTParms(parms)
 {
+}
+
+void CctwqtCrystalCoordinateTransform::report()
+{
+  printMessage(tr("CctwqtCrystalCoordinateParameters - get_Wavelength() = %1")
+               .arg(m_QTParms->get_Wavelength()));
+
+  printMessage(tr("CctwCrystalCoordinateParameters::m_Wavelength = %1")
+               .arg(((CctwCrystalCoordinateParameters*) m_QTParms)->m_Wavelength));
 }
--- cctwqt/cctwqtcrystalcoordinatetransform.h
+++ cctwqt/cctwqtcrystalcoordinatetransform.h
@@ -3,17 +3,22 @@
 
 #include "cctwqtobject.h"
 #include "../cctwcrystalcoordinatetransform.h"
+#include "cctwqtcrystalcoordinateparameters.h"
 
 class CctwqtCrystalCoordinateTransform : public CctwCrystalCoordinateTransform
 {
   Q_OBJECT
 public:
-  explicit CctwqtCrystalCoordinateTransform(CctwCrystalCoordinateParameters *parms, QObject *parent);
+  explicit CctwqtCrystalCoordinateTransform(CctwqtCrystalCoordinateParameters *parms, QObject *parent);
   
 signals:
   
 public slots:
   double changeCount() { return CctwCrystalCoordinateTransform::m_CurrentFrameChangeCount; }
+  void report();
+
+private:
+  CctwqtCrystalCoordinateParameters *m_QTParms;
 };
 
 #endif // CCTWQTCRYSTALCOORDINATETRANSFORM_H
