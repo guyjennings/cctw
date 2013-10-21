3accc378127b1a998e549375bf1afd9167ecb2a1
jennings <jennings@anl.gov>
2013-10-21 16:28:08 -0500

    Added test script for tcl interface

---
+++ cctwtcl/cctwtcltest.tcl
@@ -0,0 +1,8 @@
+package require cctw
+
+cctw {
+  outputData.dimensions = [128*3, 128*6, 128*4]
+  outputDataManager.filePath = "./h5test.h5"
+  cctw.calculateDependencies()
+  transformer.transform()
+}
