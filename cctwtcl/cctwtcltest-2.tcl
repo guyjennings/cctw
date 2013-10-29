bdbfccc497c6f6982d41ef90639e8bb77c1a2504
Justin M Wozniak <wozniak@mcs.anl.gov>
2013-10-29 09:36:46 -0500

    Sketch of parallelizable loop

--- cctwtcl/cctwtcltest-2.tcl
+++ cctwtcl/cctwtcltest-2.tcl
@@ -2,9 +2,20 @@ package ifneeded cctw 0.1 "load ./libCctwTcl.so cctw"
 
 package require cctw
 
-cctw {
-  outputData.dimensions = [128*3, 128*6, 128*4]
-  outputDataManager.filePath = "./h5test.h5"
-  cctw.calculateDependencies()
-  transformer.transform()
+# Initialize CCTW
+cctw_init
+
+# Query CCTW for the number of chunks
+set maxChunk [ cctw_maxchunks ]
+
+for { set chunkID 0 } { $chunkID < $maxChunks } { incr chunkID } { 
+
+    # Set the current chunk
+    cctw_chunk $chunkID
+    cctw {
+        outputData.dimensions = [128*3, 128*6, 128*4]
+        outputDataManager.filePath = "./h5test.h5"
+        cctw.calculateDependencies()
+        transformer.transform()
+    }
 }
