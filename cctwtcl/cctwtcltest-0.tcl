9a4bce7b8ecb59449aa24810616410aed59acf0d
Justin M Wozniak <wozniak@mcs.anl.gov>
2013-10-29 13:14:07 -0500

    Sanity test for return values

---
+++ cctwtcl/cctwtcltest-0.tcl
@@ -0,0 +1,14 @@
+package ifneeded cctw 0.1 "load ./libCctwTcl.so cctw"
+
+package require cctw
+
+set result [ cctw { 42 } ] 
+
+exit [ expr ! ( $result == 42 ) ]
+
+# cctw {
+#   outputData.dimensions = [128*3, 128*6, 128*4]
+#   outputDataManager.filePath = "./h5test.h5"
+#   cctw.calculateDependencies()
+#   transformer.transform()
+# }
