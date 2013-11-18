a8bb3e88f98901ed71bbf7056f74924e98aff2f6
jennings <jennings@anl.gov>
2013-11-18 17:18:48 -0600

    Descriptions of the tcl commands

--- cctwtcl/cctwtcl_commands.h
+++ cctwtcl/cctwtcl_commands.h
@@ -19,5 +19,108 @@ int Cctwtcl_Output_Cmd        (ClientData clientData, Tcl_Interp *interp, int ob
 int Cctwtcl_Delete_Cmd        (ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]);
 int Cctwtcl_Blob_Cmd          (ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]);
 
+/*
+ *  Commands to implement a basic interface to cctw from swift/t via tcl
+ *
+ *  The following tcl commands are implemented:
+ *
+ *  cctw <cmd>
+ *
+ *    Passes javascript commands direct to the cctew script interpreter
+ *
+ *    Example:   cctw inputData.chunkSize
+ *          -> 128,128,128
+ *  cctw_count
+ *
+ *    Returns a list of two integers - the number of chunks in the input and output
+ *    datasets respectively
+ *
+ *  cctw_input <inputchunkid> <filePath>
+ *
+ *    Reads a chunk of input data, normalizes it and performs any necessary corrections
+ *    The filePath is interpreted as a URI - though the precise semantics are still to
+ *    be determined.
+ *
+ *    Returns a list of three integers - the chunkid, the chunk length in bytes and
+ *    the location of the chunk
+ *
+ *    Example:   cctw_input 1234 h5:example.h5/ffscan
+ *          ->   1234 33554472 6721372160
+ *               cctw_input 1235 h5:example.h5/ffscan
+ *          ->   1235 33554472 6889164800
+ *
+ *  cctw_transform <inputchunkid> <blob>
+ *
+ *    Performs a cctw transform of a chunk of input data to produce a list of intermediate
+ *    transformed output chunks.
+ *
+ *    Returns a list of lists of three integers - the chunkid, lengh and location of each intermediate chunk
+ *
+ *    Example:   cctw_transform 1234 6721372160
+ *          -> {48 33554472 6754930688} {304 33554472 6788489216} {305 33554472 6822047744} {560 33554472 6855606272}
+ *               cctw_transform 1235 6889164800
+ *          -> {48 33554472 6922723328} {304 33554472 6956281856} {560 33554472 6989840384}
+ *
+ *  cctw_merge <outputchunkid> <blob1> <blob2>
+ *
+ *    Merges together two intermediate chunks with the same chunk id to produce a summed chunk
+ *
+ *    Returns a list of three integers - the chunkid, the chunk length in bytes and
+ *    the location of the chunk
+ *
+ *    Example:   cctw_merge 304 6788489216 6956281856
+ *          -> 304 33554472 7023398912
+ *
+ *  cctw_normalize <outputchunkid> <blob>
+ *
+ *    Normalizes an intermediate chunk by dividing the data values by the weight values and produces an output chunk
+ *
+ *    Returns a list of three integers - the chunkid, length and location of the output chunk
+ *
+ *    Example:   cctw_normalize 304 7023398912
+ *          -> 304 16777256 7056957440
+ *
+ *  cctw_output <outputchunkid> <filePath> <blob>
+ *
+ *    Writes an output blob of data into the output file
+ *    The filePath is interpreted as a URI - semantics to be determined
+ *
+ *    Example:  cctw_output 304 h5:example.h5/output
+ *
+ *  cctw_delete <chunkid> <blob>
+ *
+ *    Should be called to delete each allocated blob
+ *
+ *    The chunkid should be an input or output chunkid as appropriate
+ *
+ *    Example: cctw_delete 304 7023398912
+ *
+ *  cctw_blob <chunkid> <blob>
+ *
+ *    Can be used to extract data out of a cctw blob into a tcl binary array
+ *
+ *    Returns three values - the chunkid, the length and the binary array
+ *
+ *    The binary array argument is suitable for use with the tcl binary command
+ *
+ *    Example: set x [cctw_blob 48 6754930688]
+ *         -> 48 33554472 {0ÂÂÂ  }
+ *             binary scan [lindex $x 2] iiiiii a b c d e f
+ *         -> 6
+ *             puts $d
+ *         -> 128
+ *
+ *    The blobs have a common format - the first 7 integer values are
+ *      0:  blob type    0 = input, 1 = intermediate, 2 = output
+ *      1:  blob id
+ *      2:  blob x dimension
+ *      3:  blob y dimension
+ *      4:  blob z dimension
+ *      5:  number of data elements
+ *      6:  number of weight elements (or 0 for an output blob)
+ *
+ *    The blob data then follows as an array of doubles
+ *
+ **/
 
 #endif // CCTWTCL_COMMANDS_H
