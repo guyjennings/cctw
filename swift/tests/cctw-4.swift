e9cdbb9c9af3be42adc64f40801a0056f1fd2a22
jwozniak <wozniak@mcs.anl.gov>
2013-11-19 16:37:28 -0700

    Uncomment more of program

--- swift/tests/cctw-4.swift
+++ swift/tests/cctw-4.swift
@@ -10,25 +10,22 @@ import cctw;
 
 main
 {
-  // QList<void*> = hdf_read(string name)
-
-  // E.g. fragment[(1,1,1)] = <binary data>
-
   blob fragment[];
 
-  int max_i = 2;
-  int max_j = 2;
-  int max_k = 2;
+  int input_chunks_i = 2;
+  int input_chunks_j = 2;
+  int input_chunks_k = 2;
 
-  foreach i in [0:max_i-1] {
-    foreach j in [0:max_j-1] {
-      foreach k in [0:max_k-1] {
+  foreach i in [0:input_chunks_i-1] {
+    foreach j in [0:input_chunks_j-1] {
+      foreach k in [0:input_chunks_k-1] {
         string hdf_file = sprintf("fragment-%i-%i-%i.hdf", i, j, k);
-        int id = cctw_ijk2id(max_i,max_j,max_k,i,j,k);
+        int id = cctw_ijk2id(input_chunks_i,input_chunks_j,
+                             input_chunks_k,i,j,k);
         printf("opening: %s for chunk id: %i", hdf_file, id);
         // fragment[] = read_hdf_fragment(hdf_file, i,j,k);
       }}}
-/*
+
   output_chunks_x = 16; // toint(argv("chunks_x"));
   output_chunks_y = 16; // toint(argv("chunks_y"));
   output_chunks_z = 16; //
@@ -41,26 +38,8 @@ main
   // input chunk IDs needed for output chunk (index)
   int requisite_input_chunk_IDs[][];
 
-  // (1,1,1) -> (0,3,0), (0,3,1), (1,3,1)
-  // (2,2,2) -> (0,5,0), (0,7,3), (1,3,1)
-  // requisite_input_chunk_IDs[(1,3,1)] = (1,1,1), (2,2,2)
-
-  output_products[][];
-
-  // foreach i in [0:max_input_x-1] {
-  //   foreach j in [0:max_input_y-1] {
-  //     foreach k in [0:max_input_z-1] {
-
-  //       int output_chunk_IDs[] = forward_dependencies(i,j,k);
-  //       foreach id in output_chunk_IDs {
-  //         requisite_input_chunk_IDs[id] += (i,j,k);
-  //       }
-  //       // if (condition)
-  //       //   input_chunks_needed += (i,j,k);
-  //     }}}
-
-
-  // C++:
+  blob output_products[][];
+  /*  // C++:
   // List<(i,j,k,double* intensity, double* weight)> =
   // forward_transform(i,j,k, double* intensity (IN), double* weight (IN),
   foreach id in input_chunks {
