82e310394cc934aa498a8d58d218a289d3c40cee
Justin M Wozniak <wozniak@mcs.anl.gov>
2014-05-06 15:12:54 -0500

    Read many blobs

--- swift/tests/cctw-2.swift
+++ swift/tests/cctw-2.swift
@@ -11,8 +11,10 @@ import cctw;
 
 main
 {
-  
-  blob tiff = cctw_input(getenv("HOME")/"nexus-data/pznpt.nxs",
-                         "entry/data/v", 1);
-  printf("blob_size: %i", blob_size(tiff));
+  foreach i in [0:9]
+  {
+    blob b = cctw_input(getenv("HOME")/"nexus-data/pznpt.nxs",
+                        "entry/data/v", i);
+    printf("blob_size: %i", blob_size(b));
+  }
 }
