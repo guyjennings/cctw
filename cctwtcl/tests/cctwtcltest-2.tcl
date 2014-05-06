f00749173a4c4433a6e7b182d6a0a67a8f4ad860
Justin M Wozniak <wozniak@mcs.anl.gov>
2014-05-06 09:39:58 -0500

    New input call

--- cctwtcl/tests/cctwtcltest-2.tcl
+++ cctwtcl/tests/cctwtcltest-2.tcl
@@ -1,14 +1,9 @@
 
 package require cctw
 
-set chunk [ cctw_chunk_create 128 ]
-cctw_chunk_read "pznpt4_0070.tif" pointer length
+set L [ cctw_input "pznpt4_0070.tif" 1 ] 
+set pointer [ lindex $L 0 ] 
+set length  [ lindex $L 1 ] 
 
 puts "pointer: $pointer length: $length"
 
-# cctw {
-#   outputData.dimensions = [128*3, 128*6, 128*4]
-#   outputDataManager.filePath = "./h5test.h5"
-#   cctw.calculateDependencies()
-#   transformer.transform()
-# }
