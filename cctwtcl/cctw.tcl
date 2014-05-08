ca7d2061e9dfea16e211d7e737a93ffd76268e61
Justin M Wozniak <wozniak@mcs.anl.gov>
2014-05-08 12:05:34 -0500

    Partially complete rule for cctw_transform

--- cctwtcl/cctw.tcl
+++ cctwtcl/cctw.tcl
@@ -8,4 +8,18 @@ namespace eval cctw {
         set length [ lindex $L 1 ]
         return [ list $pointer $length ]
     }
+
+    proc cctw_transform_tcl { outputs inputs } { 
+        puts "cctw_transform_tcl: $outputs $inputs"
+        rule $inputs "cctw::cctw_transform_tcl_body $outputs $inputs"
+    }
+    
+    proc cctw_transform_tcl_body { output_ids output_blobs input_id input_blob } { 
+        set input_id_value   [ retrieve_integer $input_id   ] 
+        set input_blob_value [ retrieve_blob    $input_blob ] 
+        show input_id_value
+        set ptr [ lindex $input_blob_value 0 ]
+        set products [ cctw_transform $ptr $input_id_value ]
+        show products
+    }
 }
